3583168f8d8fd015e965c1cdbbbcd2eee1023d9f
Kamil Bęben <kbeben@puretech.com.pl>
2023-06-12 09:00:40 +0200

    NotGate

---
+++ src/main/java/pl/beben/computersimulation/device/booleanfunction/gate/NotGate.java
@@ -0,0 +1,38 @@
+package pl.beben.computersimulation.device.booleanfunction.gate;
+
+import pl.beben.computersimulation.device.booleanfunction.LogicGate;
+import pl.beben.computersimulation.device.misc.InputBinder;
+
+public class NotGate extends LogicGate {
+
+  public NotGate(String id) {
+    super(
+      id, 1,
+      """
+      0 1
+      1 0
+      """
+    );
+
+    //
+    //     `inputBinder`
+    //           │
+    //           │  ┌─ `nandGate`
+    //           │  │
+    //           ↓  ↓
+    //           .--|````|
+    // (input)---|  |NAND|---(output)
+    //           `--|....|
+    //
+
+    final var nandGate = new NandGate(id + "#inputBinder#nand");
+    final var inputBinder = new InputBinder(this, "inputBinder");
+
+    nandGate.getInput(0).connectTo(inputBinder);
+    nandGate.getInput(1).connectTo(inputBinder);
+
+    inputs[0] = inputBinder;
+    outputs[0] = nandGate;
+  }
+
+}
